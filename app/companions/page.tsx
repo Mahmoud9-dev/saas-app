196b7ae8d7561fa637bc3f6e80b6822283384e4f
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-06-25 15:53:49 +0300

    Implement Companion Library, Search Bar

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import { getAllCompanions } from "@/lib/actions/companion.action";
+import CompanionCard from "@/components/CompanionCard";
+import { getSubjectColor } from "@/lib/utils";
+import SearchInput from "@/components/SearchInput";
+import SearchFilter from "@/components/SearchFilter";
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
 
-const CompanionsLibrary = () => {
-    return (
-        <div>CompanionsLibrary</div>
-    )
-}
-export default CompanionsLibrary
+  const companions = await getAllCompanions({
+    subject,
+    topic,
+  });
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companions Library</h1>
+        <div className="flex gap-4">
+          <SearchInput />
+          <SearchFilter />
+        </div>
+      </section>
+
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
+};
+export default CompanionsLibrary;
