e72f3cb9444e486e8c0d83997172186ba4965ee5
Mahmoud-dev09 <mahmoud0114829@gmail.com>
2025-06-30 20:35:26 +0300

    Change Org and Project Names

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,26 +5,26 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://4d7ec5ef63172bd72b3eb7f6536aef1c@o4507778758344704.ingest.us.sentry.io/4509562157006848",
+    dsn: "https://4d7ec5ef63172bd72b3eb7f6536aef1c@o4507778758344704.ingest.us.sentry.io/4509562157006848",
 
-  // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+    // Add optional integrations for additional features
+    integrations: [
+        Sentry.replayIntegration(),
+    ],
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+    tracesSampleRate: 1,
 
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+    // Define how likely Replay events are sampled.
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
 
-  // Define how likely Replay events are sampled when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
+    // Define how likely Replay events are sampled when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "mndpro",
+  org: "self-employed-mhz",
   project: "mnd_converso",
 
   // Only print logs for uploading source maps in CI
